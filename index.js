51d50e2c03fbd8b24902f28a1a9783444859c11d
Kaushik Pawar <kaushikpawar87@gmail.com>
2022-02-15 10:00:09 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Create random Number 1-6
+var randomNumber1 = Math.floor(Math.random()*6 + 1);
+var randomNumber2 = Math.floor(Math.random()*6 + 1);
+
+// select the Image element
+var img1 = document.querySelector(".img1");
+var img2 = document.querySelector(".img2");
+
+// Assign the Image
+img1.setAttribute("src", "images/dice"+randomNumber1+".png");
+img2.setAttribute("src", "images/dice"+randomNumber2+".png");
+
+// Change the h1 text
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if(randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else{
+  document.querySelector("h1").innerHTML = "Draw!";
+};
